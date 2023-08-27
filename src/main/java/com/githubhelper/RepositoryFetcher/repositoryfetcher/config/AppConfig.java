3e71d0ff8f8656aca61bbdb95592f4a671c5212d
broslaws <pawelgawrych203@gmail.com>
2023-08-27 16:36:24 +0200

    Remove unnecessary files

--- src/main/java/com/githubhelper/RepositoryFetcher/repositoryfetcher/config/AppConfig.java
+++
@@ -1,8 +0,0 @@
-package com.githubhelper.RepositoryFetcher.repositoryfetcher.config;
-
-import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.PropertySource;
-
-@Configuration
-@PropertySource("classpath:githubApiEndpoints.properties")
-public class AppConfig {}
--- src/main/java/com/githubhelper/RepositoryFetcher/repositoryfetcher/connector/ApiConnector.java
+++
@@ -1,6 +0,0 @@
-package com.githubhelper.RepositoryFetcher.repositoryfetcher.connector;
-
-public interface ApiConnector {
-
-    String getJsonFromAPI(String url);
-}
